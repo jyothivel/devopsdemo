784762bce4f233008414c042c5134cec39376516
EC2 Default User <ec2-user@ip-172-12-104-221.ec2.internal>
2019-06-05 14:42:14 +0000

    Customer update

--- devopsdemo/index.php
+++ devopsdemo/index.php
@@ -159,7 +159,7 @@ if (isset($_SESSION['username']))
 	// This section is shown when user is login
 	echo "<table width=100% border=0>";
 	echo "<tr>";
-		echo "<td><H1>Demo Application for Charlie</H1></td>";
+		echo "<td><H1>Demo Application for Avaya</H1></td>";
 		echo "<td align='right'>";
 			echo "$username<br>";
 			echo "<a href='demo/index.php?logout=yes'>Logout</a>";
